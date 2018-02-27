8be83f376307cfb87dfaa5d10ae50271b3c9e812
juiwei830401 <31119875+juiwei830401@users.noreply.github.com>
2018-02-27 15:31:20 +0800

    Update FIVE.js

--- FIVE.js
+++ FIVE.js
@@ -3,7 +3,18 @@ app.controller('FIVE', function ($rootScope, $scope, $controller, $filter) {
 	//初始
 	$scope.init = function(){
 		$scope.list = [];
+		$scope.grid = [];
 		
+		//--棋盤格子
+		//Y軸
+		for(var i = 0; i < 18; i++){
+			$scope.grid.push({dataY : [], row : i});
+			//X軸
+			for(var j = 0; j < 18; j++){
+				$scope.grid[i].dataY.push({dataX : j, STATUS : ''});
+			}
+		}
+		//--棋盤欄位
 		//Y軸
 		for(var i = 0; i < 19; i++){
 			$scope.list.push({dataY : [], row : i});
@@ -12,6 +23,7 @@ app.controller('FIVE', function ($rootScope, $scope, $controller, $filter) {
 				$scope.list[i].dataY.push({dataX : j, STATUS : ''});
 			}
 		}
+		
 		$scope.OX = 'O';
 		$scope.result = '';
 		$scope.lock = false;
@@ -30,49 +42,59 @@ app.controller('FIVE', function ($rootScope, $scope, $controller, $filter) {
 				$scope.list[Y].dataY[i].STATUS = $scope.OX;
 				
 				//判斷連線
-				var checkA = 0;	/**	判斷:-	**/
-				var checkB = 0;	/**	判斷:|	**/
-				var checkC = 0;	/**	判斷:/	**/
-				var checkD = 0;	/**	判斷:\	**/
+				var checkA = 0;	/**	判斷 : 「-」	**/
+				var checkB = 0;	/**	判斷 : 「|」	**/
+				var checkC = 0;	/**	判斷 : 「/」	**/
+				var checkD = 0;	/**	判斷 : 「\」	**/
 				
 				for(var j = -4; j < 5; j++){
 					if(Number(i + j) >= 0 && Number(i + j) < 19){
 						if($scope.list[Y].dataY[i + j].STATUS == $scope.OX){
 							checkA++;
+						}else{
+							checkA = 0;
 						}
 					}
 					if(Number(Y + j) >= 0 && Number(Y + j) < 19){
 						if($scope.list[Y + j].dataY[i].STATUS == $scope.OX){
 							checkB++;
+						}else{
+							checkB = 0;
 						}
 					}
 					if(Number(i + j) >= 0 && Number(Y + j) >= 0 && Number(i + j) < 19 && Number(Y + j) < 19){
 						if($scope.list[Y + j].dataY[i + j].STATUS == $scope.OX){
 							checkC++;
+						}else{
+							checkC = 0;
 						}
 					}
 					if(Number(i - j) >= 0 && Number(Y + j) >= 0 && Number(i - j) < 19 && Number(Y + j) < 19){
 						if($scope.list[Y + j].dataY[i - j].STATUS == $scope.OX){
 							checkD++;
+						}else{
+							checkD = 0;
 						}
 					}
 				}
+				//結束
 				if(checkA >= 5 || checkB >= 5 || checkC >= 5 || checkD >= 5){
 					$scope.result = $scope.OX + ' WIN';
+					alert($scope.result);
 					$scope.lock = true;
 				}
-				
-				//OX調換
-				if($scope.OX == 'O'){
-					$scope.OX = 'X';
-				}else if($scope.OX == 'X'){
-					$scope.OX = 'O';
-				}
-				return;
+				//調換
+				$scope.change();
 			}
 		}
-		
-
 	}
-
+	
+	//OX調換
+	$scope.change = function(){
+		if($scope.OX == 'O'){
+			$scope.OX = 'X';
+		}else if($scope.OX == 'X'){
+			$scope.OX = 'O';
+		}
+	}
 });
